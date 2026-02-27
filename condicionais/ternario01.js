3a4f19ee570f464a70038a5455a803f50ba63f2a
williammatheus005 <williammatheuslimacordeiro@gmail.com>
2026-02-27 13:58:25 +0000

    cod

---
+++ condicionais/ternario01.js
@@ -0,0 +1,12 @@
+let solteiro = true
+
+if (solteiro == true) {
+    console.log("vem novinha!!!")
+}
+else {
+    console.log("vai se embora rapariga!!!")
+}
+
+const estadoCivil = solteiro == true
+? "vem novinha!!!" : "vai se embora rapariga!!!"
+console.log(estadoCivil)
