b21240865284b791b1a6701c10bbe5137f50a831
williammatheus005 <williammatheuslimacordeiro@gmail.com>
2026-03-27 13:31:52 +0000

    funcao 02

--- funcoes/func-02.js
+++ funcoes/func-02.js
@@ -14,14 +14,14 @@ function soma(n3, n4) {
     console.log("a soma de " + n3 + " por " + n4 + " é igual a " + (n3 + n4))
 } 
 
-soma(5,5)
+soma(5,15)
 
 function subtracao(n5, n6) {
 
     console.log("a diferença de " + n5 + " por " + n6 + " é igual a " + (n5 - n6))
 } 
 
-subtracao(70,20)
+subtracao(70,2)
 
 function multiplicacao(n7, n8) {
 
