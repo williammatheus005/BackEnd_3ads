4fa569b67d5c007ca3176e3769bc895f417cb086
williammatheus005 <williammatheuslimacordeiro@gmail.com>
2026-08-14 11:33:29 +0000

    adicionar página de erro 404 e atualizar rotas de navegação

--- 2 TRI/modulos/mod-server03/server01.js
+++ 2 TRI/modulos/mod-server03/server01.js
@@ -7,6 +7,7 @@ const porta = 8001
 
 const home = path.join(__dirname, '/pages/index.html')
 const compras = path.join(__dirname, '/pages/compras.html')
+const erro = path.join(__dirname, '/pages/erro.html')
 
 // criar servidor e rotas
 const server = http.createServer((req, res) => {
@@ -20,6 +21,10 @@ const server = http.createServer((req, res) => {
     if (caminhoPesquisado === '/compras') {
         res.setHeader('Content-Type', 'text/html; charset=utf8')
         return res.end(fs.readFileSync(compras, 'utf8'))
+    } 
+    if (caminhoPesquisado === '/erro') {
+        res.setHeader('Content-Type', 'text/html; charset=utf8')
+        return res.end(fs.readFileSync(erro, 'utf8'))
     } else {
         res.setHeader('Content-Type', 'text/html; charset=utf8')
         res.end('<h3>Página não permitida</h3>')
